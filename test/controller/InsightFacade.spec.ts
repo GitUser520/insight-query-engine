6c930cf07156f7ce0f5bc0fdc8082dbd043807fe
Ray Yang <ryang07@student.ubc.ca>
2022-02-02 15:21:22 -0800

    add c0 tests

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,10 +1,11 @@
 import {
 	InsightDatasetKind,
 	InsightError,
-	InsightResult,
+	InsightResult, NotFoundError,
 	ResultTooLargeError
 } from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
+import {clearDisk, getContentFromArchives} from "../TestUtil";
 
 import * as fs from "fs-extra";
 
@@ -42,6 +43,7 @@ describe("InsightFacade", function () {
 			// This section resets the insightFacade instance
 			// This runs before each test
 			console.info(`BeforeTest: ${this.currentTest?.title}`);
+			clearDisk();
 			insightFacade = new InsightFacade();
 		});
 
@@ -65,6 +67,155 @@ describe("InsightFacade", function () {
 				expect(result).to.deep.equal(expected);
 			});
 		});
+
+		it("should not add one dataset given invalid dataset", async function () {
+			const contentInvalid = getContentFromArchives("invalid.txt");
+			try {
+				await insightFacade.addDataset("courses", contentInvalid, InsightDatasetKind.Courses);
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("should not add one dataset given invalid id with underscore", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			try {
+				await insightFacade.addDataset("courses_2", content, InsightDatasetKind.Courses);
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("should not add one dataset given invalid id empty string", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			try {
+				await insightFacade.addDataset("", content, InsightDatasetKind.Courses);
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("should not add one dataset given a valid dataset that was already added", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			try {
+				await insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+			} catch (err) {
+				expect.fail("cannot add dataset");
+			}
+			try {
+				await insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("should list no dataset", function () {
+			return insightFacade.listDatasets().then((insightDatasets: any ) => {
+				expect(insightDatasets).to.deep.equal([]);
+				// or:
+				expect(insightDatasets).to.be.an.instanceof(Array);
+				expect(insightDatasets).to.have.length(0);
+			});
+		});
+
+		it("should list one dataset", function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			return insightFacade.addDataset("courses", content, InsightDatasetKind.Courses)
+				.then(() => insightFacade.listDatasets())
+				.then((insightDatasets) => {
+					expect(insightDatasets).to.deep.equal([{
+						id: "courses",
+						kind: InsightDatasetKind.Courses,
+						numRows: 64612,
+					}]);
+
+					// or:
+					expect(insightDatasets).to.be.an.instanceof(Array);
+					expect(insightDatasets).to.have.length(1);
+					const [insightDataset] = insightDatasets;
+					expect(insightDataset).to.have.property("id");
+					expect(insightDataset.id).to.equal("courses");
+					expect(insightDataset).to.have.property("kind");
+					expect(insightDataset.kind).to.equal(InsightDatasetKind.Courses);
+					expect(insightDataset).to.have.property("numRows");
+					expect(insightDataset.numRows).to.equal(64612);
+
+
+				});
+
+		});
+
+		it("should list multiple datasets", function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			return insightFacade.addDataset("courses", content, InsightDatasetKind.Courses)
+				.then(() => {
+					return insightFacade.addDataset("courses-2", content, InsightDatasetKind.Courses);
+				})
+				.then(() => {
+					return insightFacade.listDatasets();
+				})
+				.then((insightDatasets) => {
+					expect(insightDatasets).to.be.an.instanceof(Array);
+					expect(insightDatasets).to.have.length(2);
+					const insightDatasetCourses = insightDatasets.find((dataset) => dataset.id === "courses");
+					expect(insightDatasetCourses).to.exist;
+					expect(insightDatasetCourses).to.deep.equal({
+						id: "courses",
+						kind: InsightDatasetKind.Courses,
+						numRows: 64612,
+					});
+				});
+
+		});
+
+		it("should remove one dataset given valid id", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			try {
+				await insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+			} catch (err) {
+				expect.fail("cannot add dataset");
+			}
+			return insightFacade.removeDataset("courses").then((removed: any ) => {
+				expect(removed).to.equal("courses");
+			});
+		});
+
+		it("should not remove one dataset given valid id not yet added", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+
+			try {
+				await insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+			} catch (err) {
+				expect.fail("cannot add dataset");
+			}
+			try {
+				await insightFacade.removeDataset("cou");
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(NotFoundError);
+			}
+		});
+
+		it("should not remove one dataset given invalid id", async function () {
+			const content: string = datasetContents.get("courses") ?? "";
+			try {
+				await insightFacade.addDataset("courses", content, InsightDatasetKind.Courses);
+			} catch (err) {
+				expect.fail("cannot add dataset");
+			}
+			try {
+				await insightFacade.removeDataset("courses_1");
+				expect.fail("should have thrown an error");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+
 	});
 
 	/*
